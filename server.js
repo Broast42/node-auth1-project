08be086add6ef729118735416688a87d52d05a52
rdowd <rjarrettdowd@yahoo.com>
2020-05-06 19:24:23 -0400

    created server and db to store usernames and passwords.

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express')
+
+const server = express()
+
+server.use(express.json())
+
+//routes here
+
+//default error
+server.use((err, req, res, next) => {
+	console.log(err)
+	res.status(500).json({
+		message: "Something went wrong",
+	})
+})
+
+module.exports = server
